d45eaff4c2521e115c1988dd978b1366ce644534
user <nasreen94>
2020-01-14 11:45:16 +0530

    first commit

---
+++ arithmetic/arithmetic .py
@@ -0,0 +1,17 @@
+no1=1
+no2=2
+no3=no1+no2
+no4=no1-no2
+no5=no1*no2
+no6=no1/no2
+no7=no1//no2
+no8=no1**no2
+print("addition result =",no3)
+print("substraction =",no4)
+print("float division result=",no7)
+print("power result",no8)
+
+#swapping
+(no1,no2)=(no2,no1)
+print(no1)
+print(no2)
