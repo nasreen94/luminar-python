94b74781aa9599b7b340c4666cf99593471c1a83
“Reshma” <reshmaphilip97@gmail.com>
2020-01-16 22:16:33 +0530

    fourth commit

---
+++ flow contols/primeinrange.py
@@ -0,0 +1,19 @@
+
+# for i in range(2,101):
+#     for j in range(2,i):
+#         if(i%j==0):
+#             break
+#     else:
+#         print(i)
+
+
+
+sum=0
+for i in range(2,101):
+    for j in range(2,i):
+        if(i%j==0):
+            break
+    else:
+        #print(i)
+        sum+=i
+print(sum)
